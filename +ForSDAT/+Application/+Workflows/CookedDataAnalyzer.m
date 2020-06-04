025599d1cb3dd67c011d653acd9ed97e05fed016
TaDuAs <47486731+TaDuAs@users.noreply.github.com>
2020-06-04 11:47:02 +0300

    Update CookedDataAnalyzer.m

--- +ForSDAT/+Application/+Workflows/CookedDataAnalyzer.m
+++ +ForSDAT/+Application/+Workflows/CookedDataAnalyzer.m
@@ -11,6 +11,7 @@ classdef (Abstract) CookedDataAnalyzer < handle
                                     % Li et al. Langmuir 2014, https://doi.org/10.1021/la501189n
                                     % in this article they report using
                                     % A=10^6, but that is a misprint, its in fact 10^-6Hz 
+        BellEvansAlpha = 0.95;
     end
         
     % 
@@ -271,22 +272,22 @@ classdef (Abstract) CookedDataAnalyzer < handle
             ylabel({'MPF (pN)'}, 'FontSize', 24);
 
             % Create textbox
-            textbox = annotation(fig,'textbox',...
-                [0.15 0.72 0.20 0.19],...
-                'String',{['R^2 = ', num2str(round(R2, 4))],...
-                          [sprintf('\\chi_\\beta = %g ', params.Chi), char(197)],...
-                          sprintf('k_o_f_f = %g ª10^-^3Hz', params.Koff*1000),...
-                          sprintf('\\DeltaG = %g kJªmol^-^1', params.DeltaG/1000)},...
-                'FitBoxToText','on',...
-                'FontSize', 18);
 %             textbox = annotation(fig,'textbox',...
 %                 [0.15 0.72 0.20 0.19],...
 %                 'String',{['R^2 = ', num2str(round(R2, 4))],...
-%                           [sprintf('\chi_\beta = %g±%g ', params.Chi, params.ChiErr), char(197)],...
-%                           sprintf('k_o_f_f = %g±%g ª10^-^3Hz', params.Koff*1000, params.KoffErr*1000),...
-%                           sprintf('\DeltaG = %g±%g kJªmol^-^1', params.DeltaG/1000, params.DeltaGErr/1000)},...
+%                           [sprintf('\\chi_\\beta = %g ', params.Chi), char(197)],...
+%                           sprintf('k_o_f_f = %g ª10^-^3Hz', params.Koff*1000),...
+%                           sprintf('\\DeltaG = %g kJªmol^-^1', params.DeltaG/1000)},...
 %                 'FitBoxToText','on',...
 %                 'FontSize', 18);
+            textbox = annotation(fig,'textbox',...
+                [0.15 0.72 0.20 0.19],...
+                'String',{['R^2 = ', num2str(round(R2, 4))],...
+                          [sprintf('\\chi_\\beta = %g±%g ', params.Chi, params.ChiErr), char(197)],...
+                          sprintf('k_o_f_f = %g±%g ª10^-^3Hz', params.Koff*1000, params.KoffErr*1000),...
+                          sprintf('\\DeltaG = %g±%g kJªmol^-^1', params.DeltaG/1000, params.DeltaGErr/1000)},...
+                'FitBoxToText','on',...
+                'FontSize', 18);
         end
         
         function [params, p, R2] = bellEvansFit(this)
@@ -322,54 +323,127 @@ classdef (Abstract) CookedDataAnalyzer < handle
             
             [mpf, mpfErr, x, xErr] = prepareBellEvansData(this);
             
-            [p, S] = polyfit(x, mpf, 1);
-            R2 = util.getFitR2(mpf, S);
+%             [p, S] = polyfit(x, mpf, 1);
+%             R2 = util.getFitR2(mpf, S);
             
-            % extract kinetic values from the plot
-            slope = p(1); % KBT/chi
-            intersect = p(2);
-            secondParameter = intersect/slope; % ln(chi/KBT*Koff)
+            % use curve fitting toolbox linear fit in order to obtain
+            % coefficients confidence intervals, then backward calculate
+            % the standard error of each coefficient and extract the
+            % standard errors of the kinetic parameters
+            fitTypeObj = fittype('poly1');
+            [fitObj, gof] = fit(x(:), mpf(:), fitTypeObj);
+            R2 = gof.adjrsquare;
+            p = [fitObj.p1, fitObj.p2];
+            
+            % calculate standard errors of coefficients
+            alpha = this.BellEvansAlpha;
+            confi = confint(fitObj, alpha); % confidence intervals
+            t = tinv((1+alpha)/2, gof.dfe); % student distribution
+            polyErr = (confi(2,:)-confi(1,:)) ./ (2*t); % standard errors
+            relPolyErr = polyErr ./ p;
+            
+            % extract kinetic values from the fit
+            a = p(1); % KBT/chi
+            b = p(2); % a*ln(chi/KBT*Koff)
+            
+            % ln(chi/KBT*Koff)
+            bDiva = b/a; 
+            bDivaErr = sqrt(sum(relPolyErr.^2)) * bDiva;
+            
+            % chi/KBT*Koff
+            B = exp(bDiva);
+            BErr = bDivaErr/bDiva * B;
+            
             T = chemo.PhysicalConstants.RT; % RT in K
             heatEnergy = chemo.PhysicalConstants.kB*T;% KBT in J
             
             % Extract transition state distance/rupture distance 
-            chi = heatEnergy/slope; % in Angstoms
+            chi = heatEnergy/a; % in Angstoms
+            chiErr = relPolyErr(1)*chi;
             
             % extract dissociation rate
-            koff = exp(-secondParameter)/slope;
+            koff = 1/(B*a);
+            koffErr = sqrt(sum([BErr/B, relPolyErr(1)].^2)) * koff;
             
             % calculate the energy barrier
             Af = this.ArrheniusPrefactor;
             R = chemo.PhysicalConstants.R;
             G = R * T * log(koff/Af) / 1000; % kJ/mol
+            GErr = koffErr/koff;
             
-            params.Chi = chi;
-            params.Koff = koff;
-            params.DeltaG = G;
-            
-            % #TODO: Add error estimations
-            params.DeltaGErr = 0; 
-            params.KoffErr = 0; 
-            params.ChiErr = 0; 
+            % return parameters and standard errors
+            [params.Chi, params.ChiErr] = util.roundError(chi, chiErr);
+            [params.Koff, params.KoffErr] = util.roundError(koff, koffErr);
+            [params.DeltaG, params.DeltaGErr] = util.roundError(G, GErr);
         end
         
         function [chi, koff, refForce, deltaG] = reverseBellEvansPlot(this, repositoryNames, refLR)
-            if nargin < 3 || isempty(refLR); refLR = 5; end
+        % Calculates the Bell-Evans parametes for a list of experiment
+        % repositories.
+        % 
+        % [chi, koff, refForce, deltaG] = reverseBellEvansPlot(analyzer, repositoryNames)
+        % Calculates the Bell-Evans parameters to a list of experiment
+        % repositories (separately for each item on the list)
+        % Input:
+        %   repositoryNames - a cell array of character vectors containing 
+        %                     the list of experiment repositories
+        % Output:
+        %   chi      - A nx2 matrix containing the value of chi in the 
+        %              first column and the standard error in the second.
+        %              Chi is the distance between the bound state and
+        %              transision state.
+        %   koff     - A nx2 matrix containing the value of koff in the 
+        %              first column and the standard error in the second.
+        %              Koff is the dissociation rate.
+        %   refForce - A nx2 matrix containing the value of refForce in the 
+        %              first column and the standard error in the second. 
+        %              refForce is the rupture force of each system all
+        %              calculated for the same loading rate for reference.
+        %   deltaG   - A nx2 matrix containing the value of deltaG in the 
+        %              first column and the standard error in the second. 
+        %              DeltaG is the energy barrier between the bound state
+        %              and the transition state
+        % [___] = reverseBellEvansPlot(___, refLR)
+        % Also takes in a reference loading rate for force calculations
+        % Input:
+        %   refLR - A positive finite numeric scalar
+        %
             
-            chi = zeros(numel(repositoryNames), 1);
-            koff = zeros(numel(repositoryNames), 1);
-            deltaG = zeros(numel(repositoryNames), 1);
-            refForce = zeros(numel(repositoryNames), 1);
+            % validate input
+            if nargin < 3 || isempty(refLR); refLR = 5; end
+            mustBeNumeric(refLR);
+            mustBeFinite(refLR);
+            mustBePositive(refLR);
+            assert(isscalar(refLR), 'refLR must be a numeric scalar');
+            assert(iscellstr(repositoryNames) || isstring(repositoryNames), 'repositoryNames must be a cell array of character vectors or a string array');
+            
+            repositoryNames = cellstr(repositoryNames);
+            chi = zeros(numel(repositoryNames), 2);
+            koff = zeros(numel(repositoryNames), 2);
+            deltaG = zeros(numel(repositoryNames), 2);
+            refForce = zeros(numel(repositoryNames), 2);
             kBT = chemo.PhysicalConstants.kB * chemo.PhysicalConstants.RT;
             
             for i = 1:numel(repositoryNames)
                 this.importExperimentsRepository(repositoryNames{i});
                 
                 params = this.bellEvansFit();
-                chi(i) = params.Chi;
-                koff(i) = params.Koff;
-                deltaG(i) = params.DeltaG;
-                refForce(i) = (kBT/chi(i))*log(chi(i)*refLR/(kBT*koff(i)));
+                chi(i, :) = [params.Chi, params.ChiErr];
+                koff(i, :) = [params.Koff, params.KoffErr];
+                deltaG(i, :) = [params.DeltaG, params.DeltaGErr];
+                
+                % kbt/chi
+                af = kBT/params.Chi;
+                aerr = params.ChiErr/params.Chi;
+                
+                % ln(chi*r/kbt*koff)
+                bf = log(params.Chi*refLR/(kBT*params.Koff));
+                berr = sqrt(sum([params.ChiErr/params.Chi, params.KoffErr/params.Koff].^2));
+                
+                % calculate the force and its standard error
+                frc = af*bf;
+                frcErr = sqrt(sum([aerr/af, berr/bf].^2))*frc;
+                refForce(i, :) = [frc, frcErr];
             end
         end
     end
